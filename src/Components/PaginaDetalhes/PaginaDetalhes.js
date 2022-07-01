6d6596bfa8ff247e53dde07bbadef75bf4b94d24
Raoni Lobo <teste@MacBook-Pro-de-Raoni.local>
2022-07-01 16:42:13 -0300

    estilização página detalhe

--- src/Components/PaginaDetalhes/PaginaDetalhes.js
+++ src/Components/PaginaDetalhes/PaginaDetalhes.js
@@ -1,6 +1,9 @@
 import React from "react";
 import styled from "styled-components";
 import { createGlobalStyle } from "styled-components";
+import { Button, ButtonGroup, Stack, Icon } from "@chakra-ui/react";
+import { GiRunningNinja } from "react-icons/gi";
+import { CircularProgress, CircularProgressLabel } from '@chakra-ui/react'
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -11,66 +14,67 @@ const GlobalStyle = createGlobalStyle`
 
 const Header = styled.div`
   width: 100vw;
-  height: 10vh;
-  background-color: #ffd966;
+  height: 13vh;
+  background-color: #38b2ac;
+  letter-spacing: 3px;
 `;
 const TituloHome = styled.header`
-  font-size: 320%;
-  text-align: center;
-  justify-content: center;
+  font-size: 85px;
   font-family: "Bebas Neue";
+  color: white;
+  display: flex;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+  -webkit-text-stroke-width: 2px;
+  -webkit-text-stroke-color: black;
 `;
 
 const Container = styled.div`
+  font-size: 30px;
+  text-align: center;
+  justify-content: center;
+  width: 100vw;
+  height: 80vh;
+  background-color: #e6fffa;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: space-around;
+  padding: 35px 0;
   align-items: center;
-  gap: 10px;
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
-    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-  height: 80vh;
-  background-color: #ffe599;
+  font-family: "Tahoma";
 `;
 const Footer = styled.div`
-  width: 100vw;
-  height: 10vh;
-  background-color: #ffd966;
-`;
-
-const Button = styled.button`
-  background-color: #ffd966;
-  border: 1px solid black;
-  width: 200px;
-  height: 30px;
-  border-radius: 5px;
-  border: 0;
-  cursor: pointer;
-  &:hover {
-    background-color: #b9d3f8;
-  }
+  height: 7vh;
+  background-color: #38b2ac;
+  display: flex;
+  align-items: flex-end;
+  justify-content: end;
+  font-size: small;
+  font-family: "Tahoma";
 `;
 
 const Box = styled.div`
-  border-radius: 10px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  text-align: center;
-  gap: 4px;
+
 `;
 
 const BoxPag = styled.div`
+  overflow: hidden;
+  padding: 10px;
+  margin: 22px auto 0;
+  width: 300px;
+  font-family: Quicksand, arial, sans-serif;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.05), 0 0px 40px rgba(0, 0, 0, 0.08);
+  border-radius: 10px;
   display: flex;
-  align-items: center;
   flex-direction: column;
-  gap: 1rem;
+  align-items: center;
+  gap: 3px;
+  justify-items: center;
 `;
 
 export default class PaginaDetalhes extends React.Component {
   render() {
-
-
     const payList = this.props.servicosDetalhes.paymentMethods?.map((list) => {
       return <div key={list}> {list} </div>;
     });
@@ -87,51 +91,77 @@ export default class PaginaDetalhes extends React.Component {
       <div>
         <GlobalStyle></GlobalStyle>
         <Header>
-          <TituloHome>
-            {" "}
-            LABENINJAS{" "}
-            <img
-              src="https://cdn-icons-png.flaticon.com/512/94/94390.png?w=360"
-              alt="logo"
-              height="40px"
-              width="40px"
-            />
-          </TituloHome>
+          <TituloHome> LABENINJAS </TituloHome>
         </Header>
         <Container>
           {this.props.servicosDetalhes.title &&
           this.props.servicosDetalhes.paymentMethods ? (
             <Container>
-              <h1>{this.props.servicosDetalhes.title}</h1>
+              <h1>
+                <strong>{this.props.servicosDetalhes.title}</strong>
+              </h1>
+              <p>{this.props.servicosDetalhes.description}</p>
+              <hr/>
+              <strong>Aceita:</strong>
               <BoxPag>
-                <strong>Aceita:</strong>
+                
                 <Box>{payList}</Box>
               </BoxPag>
+              <hr/>
               <p>
                 {this.props.servicosDetalhes.dueDate &&
                   formateData(this.props.servicosDetalhes.dueDate)}{" "}
                 por <strong>R$ {this.props.servicosDetalhes.price},00</strong>
               </p>
-              <p>{this.props.servicosDetalhes.description}</p>
-
 
               {/* <Button onClick={() => this.props.updateJobTaken(this.props.AdicionarCarrinhoPaginaDetalhes.id)}> Adicionar ao carrinho</Button> */}
 
               {this.props.servicosDetalhes.taken ? (
-                <button>Adicionado</button>
+                <Button
+                  leftIcon={<Icon as={GiRunningNinja} w={8} h={8} />}
+                  colorScheme="teal"
+                  variant="solid"
+                  width={220}
+                  h={16}
+                  fontWeight="thin"
+                  fontSize="18"
+                >
+                  Adicionado
+                </Button>
               ) : (
-                <button onClick={() => this.props.updateJobTaken(this.props.AdicionarCarrinhoPaginaDetalhes)}>
+                <Button
+                  onClick={() =>
+                    this.props.updateJobTaken(
+                      this.props.AdicionarCarrinhoPaginaDetalhes
+                    )
+                  }
+                  leftIcon={<Icon as={GiRunningNinja} w={8} h={8} />}
+                  colorScheme="teal"
+                  variant="solid"
+                  width={220}
+                  h={16}
+                  fontWeight="thin"
+                  fontSize="18"
+                >
                   Adicionar ao Carrinho
-                </button>
-              )}  
-
-
-              <Button onClick={() => this.props.atualizaValor("contrato")}>
+                </Button>
+              )}
+              <Button
+                onClick={() => this.props.atualizaValor("contrato")}
+                leftIcon={<Icon as={GiRunningNinja} w={8} h={8} />}
+                colorScheme="teal"
+                variant="solid"
+                width={180}
+                h={12}
+                fontWeight="thin"
+                fontSize="18"
+              >
                 Voltar
               </Button>
+              
             </Container>
           ) : (
-            <p>Carregando...</p>
+            <CircularProgress isIndeterminate color='green.300' />
           )}
         </Container>
 
